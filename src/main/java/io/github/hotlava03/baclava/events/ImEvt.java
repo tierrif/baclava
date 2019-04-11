b710f4d7e1f02eaa008bac9916c87e680d2b8810
HotLava03 <44071180+HotLava03@users.noreply.github.com>
2019-04-11 17:34:02 +0100

    Added lower chance for sending messages
    The code is not optimized yet, lazy update

--- src/main/java/io/github/hotlava03/baclava/events/ImEvt.java
+++ src/main/java/io/github/hotlava03/baclava/events/ImEvt.java
@@ -2,7 +2,6 @@ package io.github.hotlava03.baclava.events;
 
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageChannel;
-import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
@@ -19,14 +18,29 @@ public class ImEvt extends ListenerAdapter{
         Message message = event.getMessage();
         String content = message.getContentRaw();
         if(content.toLowerCase().equals("baclava") || content.contains("<@554401969234771969>")){
-             User author = message.getAuthor();
-            MessageChannel channel = event.getChannel();
-            String userName = author.getName();
-            System.out.println("[ImEvent] " + userName + " said 'baclava' in a sentence.");
-            channel.sendMessage("what do you want").queue();
+            String[] randomizer = new String[4];
+            randomizer[0] = "1";
+            randomizer[1] = "2";
+            randomizer[2] = "3";
+            randomizer[3] = "4";
+            List<String> randomizing = Arrays.asList(randomizer);
+            Collections.shuffle(randomizing);
+            Object[] o = randomizing.toArray();
+            if(o[0] == "1")
+                event.getChannel().sendMessage("what do you want").queue();
             return;
-        }else if(content.contains("<@!362753440801095681>"))
-            event.getChannel().sendMessage("no tagging my owner thanks").queue();
+        }else if(content.contains("<@!362753440801095681>")) {
+            String[] randomizer = new String[4];
+            randomizer[0] = "1";
+            randomizer[1] = "2";
+            randomizer[2] = "3";
+            randomizer[3] = "4";
+            List<String> randomizing = Arrays.asList(randomizer);
+            Collections.shuffle(randomizing);
+            Object[] o = randomizing.toArray();
+            if (o[0] == "1")
+                event.getChannel().sendMessage("no tagging my owner thanks").queue();
+        }
         /*if((content.contains("im ")) ^ (content.contains("i'm ") ^ (content.contains("Im ") ^ (content.contains("I'm ") )))){
             if(event.getGuild().getId().equals("501535820247728138"))
                 return;
