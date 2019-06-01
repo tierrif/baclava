161e2b9479ba8bcd3d6fc4b8a6f0e4fda825f1bf
HotLava03 <44071180+HotLava03@users.noreply.github.com>
2019-06-01 01:08:40 +0100

    Code cleanup in message received events
    Lmao that's some relief on that spaghetti code

--- src/main/java/io/github/hotlava03/baclava/events/ImEvt.java
+++ src/main/java/io/github/hotlava03/baclava/events/ImEvt.java
@@ -1,13 +1,10 @@
 package io.github.hotlava03.baclava.events;
 
 import net.dv8tion.jda.core.entities.Message;
-import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class ImEvt extends ListenerAdapter{
     @Override
@@ -18,57 +15,16 @@ public class ImEvt extends ListenerAdapter{
         Message message = event.getMessage();
         String content = message.getContentRaw();
         if(content.toLowerCase().equals("baclava") || content.contains("<@554401969234771969>")){
-            String[] randomizer = new String[4];
-            randomizer[0] = "1";
-            randomizer[1] = "2";
-            randomizer[2] = "3";
-            randomizer[3] = "4";
-            List<String> randomizing = Arrays.asList(randomizer);
-            Collections.shuffle(randomizing);
-            Object[] o = randomizing.toArray();
-            if(o[0] == "1")
+            if(ThreadLocalRandom.current().nextInt(0, 10) == 0)
                 event.getChannel().sendMessage("what do you want").queue();
             return;
         }else if(content.contains("<@!362753440801095681>")) {
-            String[] randomizer = new String[4];
-            randomizer[0] = "1";
-            randomizer[1] = "2";
-            randomizer[2] = "3";
-            randomizer[3] = "4";
-            List<String> randomizing = Arrays.asList(randomizer);
-            Collections.shuffle(randomizing);
-            Object[] o = randomizing.toArray();
-            if (o[0] == "1")
+            if(ThreadLocalRandom.current().nextInt(0, 10) == 0)
                 event.getChannel().sendMessage("no tagging my owner thanks").queue();
         }
-        /*if((content.contains("im ")) ^ (content.contains("i'm ") ^ (content.contains("Im ") ^ (content.contains("I'm ") )))){
-            if(event.getGuild().getId().equals("501535820247728138"))
-                return;
-             User author = message.getAuthor();
-            MessageChannel channel = event.getChannel();
-            String userName = author.getName();
-            System.out.println("[ImEvent] " + userName + " said 'im' in a sentence.");
-            String[] splitContent = content.split("\\s");
-            splitContent[0] = "";
-            StringBuilder builder = new StringBuilder();
-            for(String s : splitContent) {
-                builder.append(s);
-            }
-            content = builder.toString();           
-            channel.sendMessage("Hi " + content + ", I'm BacLava").queue();
-        }*/
         if((content.toLowerCase().contains("suck"))){
-            MessageChannel channel = event.getChannel();
-            String[] randomizer = new String[4];
-            randomizer[0] = "1";
-            randomizer[1] = "2";
-            randomizer[2] = "3";
-            randomizer[3] = "4";
-            List<String> randomizing = Arrays.asList(randomizer);
-            Collections.shuffle(randomizing);
-            Object[] o = randomizing.toArray();
-            if(o[0] == "1")
-                channel.sendMessage("ok").queue();
+            if(ThreadLocalRandom.current().nextInt(0, 10) == 0)
+                event.getChannel().sendMessage("ok").queue();
         }
         
     }
